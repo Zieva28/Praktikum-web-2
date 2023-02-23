1e465c5193551d40fbb0c2ae38b152c2c42a2405
Zieva28 <114550005+Zieva28@users.noreply.github.com>
2023-02-23 15:16:03 +0700

    Add files via upload

---
+++ pertemuan 1/tipe_data.php
@@ -0,0 +1,19 @@
+<?php
+// String
+$name = 'Zieva';
+
+// Integer
+$umur = 17;
+
+// Float
+$ipk = 3.5;
+
+// Boolean
+$isStudent = true;
+
+// Array dibahas terpisah
+
+//menampilkan tipedata dan nilai
+var_dump($name);
+
+echo "<br> Nama saya $name Umur saya adalah $umur";
---
+++ pertemuan 1/variable.php
@@ -0,0 +1,4 @@
+<?php
+$name = "Zieva";
+
+echo 'Hai, nama saya ' . $name; 
